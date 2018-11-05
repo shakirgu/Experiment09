a47cdaa8b13ba5f8380ef174f0151cd35e06a047
md shakir <mdshakir45t@gmail.com>
2018-11-06 01:18:35 +0530

    Experiment09

---
+++ app/src/main/java/com/example/dixa/experiment9/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.dixa.experiment9;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class FirstActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void callSecondActivity(View view){
+        Intent i = new Intent(getApplicationContext(), SecondActivity.class);
+        i.putExtra("Value1", "Android By Javatpoint");
+        i.putExtra("Value2", "Simple Tutorial");
+        // Set the request code to any code you like, you can identify the
+        // callback via this code
+        startActivity(i);
+    }
+
+}
+
